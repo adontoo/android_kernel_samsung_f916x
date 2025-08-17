03cded0632d85bd654a049230eaa4d3196157579
adontoo <32664751+adontoo@users.noreply.github.com>
2025-08-17 17:18:30 +0800

    build_kernel.sh: Add time count

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+
 export ARCH=arm64
 mkdir out
 
 make -C $(pwd) -j24 O=$(pwd)/out DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y CLANG_TRIPLE=aarch64-linux-gnu- vendor/f2q_usa_singlew_defconfig
 make -C $(pwd) -j24 O=$(pwd)/out DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y CLANG_TRIPLE=aarch64-linux-gnu-
- 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
 
-
-
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date -d "$starttime" +%s)
+end_seconds=$(date -d "$endtime" +%s)
+echo 开始时间：$starttime
+echo 结束时间：$endtime
+echo 编译耗时：$((end_seconds-start_seconds))"s"
+echo 产出内核位置：out/arch/arm64/boot/Image
